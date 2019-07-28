0fdc761cf87fc7d90d051689f8722b4665b15e25
Mike <troianello@protonmail.com>
2019-07-28 02:37:24 -0400

    project cards have json CSS

--- src/components/cards/ProjectCard.js
+++ src/components/cards/ProjectCard.js
@@ -3,14 +3,12 @@ import './projectCard.css';
 
 export default function ProjectCard(props) {
   return (
-    <div>
-      <div className='project-card'>
-        <h1>
-          <a href={props.url} target='_blank' rel='noopener noreferrer'>
-            {props.title}
-          </a>
-        </h1>
+      <div className={props.category}>
+          <h1>
+            <a href={props.url} target='_blank' rel='noopener noreferrer'>
+              {props.title}
+            </a>
+          </h1>
       </div>
-    </div>
   );
 }
--- src/components/locations/NotFound.js
+++ src/components/locations/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NotFound() {
   return (
     <div>
-      <h1>THE PAGE YOU ARE LOOKING FOR DOES NOT EXIST</h1>
+      <h1>LOL NOPE</h1>
       <p>
         If you see this, you were likely attempting to break my site, so better
         luck next time!
