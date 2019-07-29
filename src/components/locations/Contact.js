6fc97566e3f925939749041bda7d742bd8f70980
Mike <troianello@protonmail.com>
2019-07-29 15:50:55 -0400

    contact card created

--- src/components/locations/Contact.js
+++ src/components/locations/Contact.js
@@ -7,38 +7,46 @@ export default function Contact() {
       <div className='row'>
         <div className='col-5 contact-side-one'>
           <h2 className='contact-center'>My Info:</h2>
-          <h3>
-            Email:{' '}
-            <strong>
-              <a
-                href='mailto:mike@troianello.co'
+          <div className="contact-side-one-box">
+            <h3>Phone: 
+            <br/><span className='contact-number'>(509)833-8340</span></h3>
+            <h3>
+            <h3>
+              Email:
+              <br/>
+              <strong>
+                <a
+                  href='mailto:mike@troianello.co'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  mike@troianello.co
+                </a>
+              </strong>
+            </h3>
+              GitHub:
+              <br/>
+              <strong>
+                <a 
+                href='https://github.com/MikeTroianello' 
+                target='_blank' 
+                rel='noopener noreferrer'>
+                  https://github.com/MikeTroianello
+                </a>
+                <br/>
+              </strong>
+            </h3>
+            <h3>
+              LinkedIn: <br/>
+              <strong>
+              <a href='https://www.linkedin.com/in/miketroianello' 
                 target='_blank'
-                rel='noopener noreferrer'
-              >
-                mike@troianello.co
+                rel='noopener noreferrer'>
+                https://www.linkedin.com/in/miketroianello
               </a>
-            </strong>
-          </h3>
-          <h3>Phone: (598)867-5309</h3>
-          <h3>
-            GitHub:{' '}
-            <strong>
-              <a 
-              href='https://github.com/MikeTroianello' 
-              target='_blank' 
-              rel='noopener noreferrer'>
-                https://github.com/MikeTroianello
-              </a>
-            </strong>
-          </h3>
-          <h3>
-            LinkedIn:
-            <a href='https://www.linkedin.com/in/miketroianello/' 
-               target='_blank'
-               rel='noopener noreferrer'>
-              https://www.linkedin.com/in/miketroianello/
-            </a>
-          </h3>
+              </strong>
+            </h3>
+          </div>
         </div>
         <div className='col-2 or'>OR</div>
         <div className='col-5 contact-side-two'>
@@ -51,7 +59,7 @@ export default function Contact() {
               <input type='email' name='email' className="contact-message-input" placeholder="Email Address"></input>
             </label>
             <label>
-              <textarea rows="4" name='content' className="contact-message-input" placeholder="Message Content"></textarea>
+              <textarea rows="7" name='content' className="contact-message-input" placeholder="Message Content"></textarea>
             </label>
             <button className="contact-button">Send</button>
           </form>
