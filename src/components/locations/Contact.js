0d78ac18e2746ee1159019faa41d3d8f399b86f6
Mike <troianello@protonmail.com>
2019-07-28 21:24:49 -0400

    Contact page links open in new tab. Direct message form introduced

--- src/components/locations/Contact.js
+++ src/components/locations/Contact.js
@@ -23,14 +23,19 @@ export default function Contact() {
           <h3>
             GitHub:{' '}
             <strong>
-              <a href='https://github.com/MikeTroianello'>
+              <a 
+              href='https://github.com/MikeTroianello' 
+              target='_blank' 
+              rel='noopener noreferrer'>
                 https://github.com/MikeTroianello
               </a>
             </strong>
           </h3>
           <h3>
             LinkedIn:
-            <a href='https://www.linkedin.com/in/miketroianello/'>
+            <a href='https://www.linkedin.com/in/miketroianello/' 
+               target='_blank'
+               rel='noopener noreferrer'>
               https://www.linkedin.com/in/miketroianello/
             </a>
           </h3>
@@ -38,6 +43,18 @@ export default function Contact() {
         <div className='col-2 or'>OR</div>
         <div className='col-5 contact-side-two'>
           <h2 className='contact-center'>Message Me Directly:</h2>
+          <form className="contact-message">
+            <label>
+              <input type='name' name='name' className="contact-message-input" placeholder="Your Name" />
+            </label>
+            <label>
+              <input type='email' name='email' className="contact-message-input" placeholder="Email Address"></input>
+            </label>
+            <label>
+              <textarea rows="4" name='content' className="contact-message-input" placeholder="Message Content"></textarea>
+            </label>
+            <button className="contact-button">Send</button>
+          </form>
         </div>
       </div>
     </div>
