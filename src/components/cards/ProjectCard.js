ed0dbe0f06faa363ae1e946f74cd9492a7db2802
Mike <troianello@protonmail.com>
2019-07-31 00:14:49 -0400

    resized project card pictures

--- src/components/cards/ProjectCard.js
+++ src/components/cards/ProjectCard.js
@@ -6,21 +6,21 @@ export default function ProjectCard(props) {
   console.log(tech);
   return (
     <div className={props.category}>
-      <h3>{props.category}</h3>
-      <h1>
-        <a href={props.url} target='_blank' rel='noopener noreferrer'>
+      <a href={props.url} target='_blank' rel='noopener noreferrer'>
+        <h3>{props.category}</h3>
+        <h1>
           {props.title}
           <img
             className='project-card-image'
             src={props.image}
             alt={props.alt}
           />
-        </a>
-        <p className='project-card-description'>{props.description}</p>
-        <ul className='project-card-list'>
-          Technologies Used: {tech.forEach(technology => {})}
-        </ul>
-      </h1>
+          <p className='project-card-description'>{props.description}</p>
+          <ul className='project-card-list'>
+            Technologies Used: {tech.forEach(technology => {})}
+          </ul>
+        </h1>
+      </a>
     </div>
   );
 }
