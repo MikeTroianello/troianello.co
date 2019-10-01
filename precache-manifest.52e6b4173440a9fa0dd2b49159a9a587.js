941a9de409a24e8a0f606c108bc2a5076279f717
Mike <troianello@protonmail.com>
2019-10-01 17:03:25 -0400

    Updates

---
+++ precache-manifest.52e6b4173440a9fa0dd2b49159a9a587.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94659f687628e7f7b27795449357d5b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae3328715e51fdb7542d",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "b521dea8473c5d14c1ed",
+    "url": "/static/css/main.20d544dd.chunk.css"
+  },
+  {
+    "revision": "ae3328715e51fdb7542d",
+    "url": "/static/js/2.2bd20bd1.chunk.js"
+  },
+  {
+    "revision": "b521dea8473c5d14c1ed",
+    "url": "/static/js/main.9e1c89bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f8e68f49f7dba3390a43f8fcc1a14f7",
+    "url": "/static/media/css3.3f8e68f4.png"
+  },
+  {
+    "revision": "e7256fcc694d1fece0727bca2f4051e8",
+    "url": "/static/media/express2.e7256fcc.png"
+  },
+  {
+    "revision": "7f130501681cb3d0129f266820c78b24",
+    "url": "/static/media/javascript.7f130501.png"
+  },
+  {
+    "revision": "b3c5133feb48eaaf72ed89f1565a622a",
+    "url": "/static/media/jquery.b3c5133f.png"
+  },
+  {
+    "revision": "ecc6f9e5d804ae0db7490c683af2f406",
+    "url": "/static/media/mongoDB2.ecc6f9e5.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "084f0cf8b8a5fdff8203d6264b1dd700",
+    "url": "/static/media/sass.084f0cf8.png"
+  }
+]);
